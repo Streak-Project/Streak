b0641a03c2bedb674a29615db1617bc7ecb50267
qkrdudwls <jacob717@ajou.ac.kr>
2025-03-11 12:56:09 +0900

    개인 설정을 위해 userDto 수정

--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -1,3 +1,5 @@
+import { IntegerType } from "typeorm";
+
 export class CreateUserDto {
     email: string;
     user_id: string;
@@ -7,4 +9,15 @@ export class CreateUserDto {
 export class LoginUserDto {
     user_id: string;
     password: string;
+}
+
+export class PersonalSettingDto {
+    password: string;
+    nickname: string;
+    sex: string;
+    address: string;
+    email: string;
+    status: IntegerType;
+    status_message: string;
+    photo: string[];
 }
